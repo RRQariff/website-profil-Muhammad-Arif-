5b5952694094c638f5d89f71f7eea51e2d762252
RRQariff <wonodadii88@gmail.com>
2024-08-05 17:30:24 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+<script>
+    function showPage(pageId) {
+        document.querySelectorAll('.page').forEach(page => {
+            page.classList.remove('active');
+        });
+        document.getElementById(pageId).classList.add('active');
+    }
+</script>
